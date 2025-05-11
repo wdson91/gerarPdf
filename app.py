63559837dfd808bbddc75ab7be67416212f80429
WdsouzaFes <wdson91@gmail.com>
2025-05-11 22:54:58 +0100

    Update app.py

--- app.py
+++ app.py
@@ -231,4 +231,4 @@ def gerar_pdf():
 
 # Executa a aplicação
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, ssl_context=("cert.pem", "key.pem"))
