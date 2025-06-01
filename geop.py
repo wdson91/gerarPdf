f61ae704e8807dccf4e55a8e2113df77cb038a32
WdsouzaFes <wdson91@gmail.com>
2025-06-01 14:15:38 +0100

    Update geop.py

--- geop.py
+++ geop.py
@@ -278,7 +278,7 @@ def search_leads(cidade, query):
     resultados = buscar_em_toda_area_multithread(coordenadas, query)
     # salvar_json(resultados, query)
     salvar_csv(resultados, query)
-    return {len(resultados), resultados}
+    return {"total": len(resultados), "resultados": resultados}
 
 
 # === EXECUÇÃO PRINCIPAL ===
